91866ed23175c5ba1c9205dd7be5a53407cfa58b
Ifeanyi Ofoefule <ifeanyiofoefule@Ifeanyis-MacBook-Pro.local>
2019-11-13 14:27:49 +0100

    start-point

---
+++ app/src/main/java/com/retrofit/activities/WelcomeActivity.kt
@@ -0,0 +1,25 @@
+package com.retrofit.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import com.retrofit.R
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_welcome)
+
+		// To be replaced by retrofit code
+		message.text = "Black Friday! Get 50% cash back on saving your first spot."
+	}
+
+	fun getStarted(view: View) {
+		val intent = Intent(this, DestinationListActivity::class.java)
+		startActivity(intent)
+		finish()
+	}
+}
